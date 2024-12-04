0de1cee36b8ea55985a82dd1573b14f7d799af84
Plague Fox <plugfox@gmail.com>
2024-12-05 01:54:42 +0400

    Update shaders

--- example/lib/src/feature/shaders/fragment_shaders_screen.dart
+++ example/lib/src/feature/shaders/fragment_shaders_screen.dart
@@ -44,7 +44,7 @@ class FragmentShadersScreen extends StatelessWidget {
             ),
             ShaderContainer(
               shader: 'simple',
-              render: (canvas, size, shader, paint) => canvas.drawRect(
+              render: (canvas, size, _, shader, paint) => canvas.drawRect(
                 Offset.zero & size,
                 paint,
               ),
@@ -52,14 +52,16 @@ class FragmentShadersScreen extends StatelessWidget {
             ShaderContainer(
               shader: 'uniforms',
               frameRate: null,
-              render: (canvas, size, shader, paint) {
+              render: (canvas, size, mouse, shader, paint) {
                 final now = DateTime.now().millisecondsSinceEpoch / 10000 % 10;
                 // clamp(abs(0.5f + 0.5f * sin(time * PI / 4.0f)), 0.0f, 1.0f)
                 //print((math.sin(now.clamp(0, 1) / (2 * math.pi))).abs());
                 shader
                   ..setFloat(0, size.width)
                   ..setFloat(1, size.height)
-                  ..setFloat(2, now);
+                  ..setFloat(2, mouse.dx)
+                  ..setFloat(3, mouse.dy)
+                  ..setFloat(4, now);
                 canvas.drawRect(
                   Offset.zero & size,
                   paint,
@@ -91,6 +93,7 @@ class ShaderContainer extends StatelessWidget {
   final void Function(
     Canvas canvas,
     Size size,
+    Offset mouse,
     ui.FragmentShader shader,
     Paint paint,
   ) render;
@@ -102,38 +105,43 @@ class ShaderContainer extends StatelessWidget {
   final int? frameRate;
 
   @override
-  Widget build(BuildContext context) => SliverPadding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        sliver: SliverToBoxAdapter(
-          child: SizedBox(
-            height: 256,
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Expanded(
-                  child: FutureBuilder<ui.FragmentShader>(
-                    initialData: null,
-                    future: ResourceManager.loadShader('shaders/$shader.frag'),
-                    builder: (context, snapshot) =>
-                        switch (snapshot.connectionState) {
-                      ConnectionState.waiting ||
-                      ConnectionState.active ||
-                      ConnectionState.none =>
-                        const Center(
-                          child: CircularProgressIndicator(),
-                        ),
-                      ConnectionState.done => switch (snapshot.data) {
-                          ui.FragmentShader fragmentShader => Stack(
-                              children: <Widget>[
-                                const Positioned.fill(
-                                  child: ColoredBox(
-                                    color: Colors.grey,
-                                    child: SizedBox.expand(),
-                                  ),
+  Widget build(BuildContext context) {
+    var mouse = Offset.zero;
+    return SliverPadding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      sliver: SliverToBoxAdapter(
+        child: SizedBox(
+          height: 256,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                child: FutureBuilder<ui.FragmentShader>(
+                  initialData: null,
+                  future: ResourceManager.loadShader('shaders/$shader.frag'),
+                  builder: (context, snapshot) =>
+                      switch (snapshot.connectionState) {
+                    ConnectionState.waiting ||
+                    ConnectionState.active ||
+                    ConnectionState.none =>
+                      const Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    ConnectionState.done => switch (snapshot.data) {
+                        ui.FragmentShader fragmentShader => Stack(
+                            children: <Widget>[
+                              const Positioned.fill(
+                                child: ColoredBox(
+                                  color: Colors.grey,
+                                  child: SizedBox.expand(),
                                 ),
-                                Positioned.fill(
+                              ),
+                              Positioned.fill(
+                                child: MouseRegion(
+                                  onHover: (event) =>
+                                      mouse = event.localPosition,
                                   child: RePaint.inline<
                                       ({
                                         ui.FragmentShader shader,
@@ -151,42 +159,45 @@ class ShaderContainer extends StatelessWidget {
                                     update: (_, state, ___) =>
                                         state..paint.blendMode = blendMode,
                                     render: (box, state, canvas) => render(
-                                        canvas,
-                                        box.size,
-                                        state.shader,
-                                        state.paint),
-                                  ),
-                                ),
-                                Positioned(
-                                  top: 8,
-                                  left: 8,
-                                  height: 24,
-                                  right: 8,
-                                  child: Align(
-                                    alignment: Alignment.topLeft,
-                                    child: Text(
-                                      shader,
-                                      maxLines: 1,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .labelLarge,
+                                      canvas,
+                                      box.size,
+                                      mouse,
+                                      state.shader,
+                                      state.paint,
                                     ),
                                   ),
                                 ),
-                              ],
-                            ),
-                          null => const Center(
-                              child: Text('Failed to load shader.'),
-                            ),
-                        },
-                    },
-                  ),
+                              ),
+                              Positioned(
+                                top: 8,
+                                left: 8,
+                                height: 24,
+                                right: 8,
+                                child: Align(
+                                  alignment: Alignment.topLeft,
+                                  child: Text(
+                                    shader,
+                                    maxLines: 1,
+                                    overflow: TextOverflow.ellipsis,
+                                    style:
+                                        Theme.of(context).textTheme.labelLarge,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        null => const Center(
+                            child: Text('Failed to load shader.'),
+                          ),
+                      },
+                  },
                 ),
-                const Divider(height: 1, color: Colors.black26, thickness: 1),
-              ],
-            ),
+              ),
+              const Divider(height: 1, color: Colors.black26, thickness: 1),
+            ],
           ),
         ),
-      );
+      ),
+    );
+  }
 }
--- example/shaders/uniforms.frag
+++ example/shaders/uniforms.frag
@@ -4,11 +4,12 @@
 precision mediump float;
 #endif
 
-//#include <flutter/runtime_effect.glsl>
+#include <flutter/runtime_effect.glsl>
 
 #define PI 3.1415926538
 
 uniform vec2 iResolution; // viewport resolution (in pixels)
+uniform vec2 iMouse; // mouse pixel coords
 uniform float iTime; // time in seconds since the shader started
 
 out vec4 fragColor;
@@ -26,9 +27,56 @@ float colorCos(float time) {
 }
 
 void main() {
-    //vec2 currentPos = FlutterFragCoord().xy;
-    float r = colorSin(iTime);
-    float g = colorCos(iTime / 4.0f);
-    float b = interpolate(0.25f, 0.75f, colorSin(iTime / 2.0f)) + 0.25f;
-    fragColor = vec4(r, g, b, 1.0f);
+    // gl_FragCoord.xy - координаты текущего пикселя в пикселях
+    // Отсчет начинается в левом нижнем углу
+    //vec2 uv = gl_FragCoord.xy / iResolution; // normalized coordinates
+
+    // Flutter меряет координаты в пикселях, а не в диапазоне от 0.0 до 1.0
+    // Отсчет начинается в левом верхнем углу
+    vec2 pos = FlutterFragCoord().xy; // current pixel coordinates
+
+    // Нормализуем координаты в диапазон от 0.0 до 1.0
+    vec2 uv = pos / iResolution; // normalized coordinates
+
+    // Цвета зависят от времени
+    float r = colorSin(iTime); // red (0.0 to 1.0)
+    float g = colorCos(iTime / 4.0f); // green (0.0 to 1.0)
+    float b = interpolate(0.5f, 1.0f, colorSin(iTime / 2.0f)); // blue (0.5 to 1.0)
+
+    // Разбиваем экран на 4 части, каждая часть будет рисовать что-то свое
+    if(uv.x < 0.5f && uv.y < 0.5f) {
+        // Верхний левый угол
+        // Заливаем градиентом зависимым от времени
+        fragColor = vec4(r, g, b, 1.0f);
+    } else if(uv.x > 0.5f && uv.y < 0.5f) {
+        // Верхний правый угол
+        // Рисум круг вокруг мыши с радиусом 5 пикселей
+        vec2 mouse = iMouse / iResolution; // normalized mouse coordinates
+        if(mouse.x > 0.5f && mouse.y < 0.5f) {
+            float dst = distance(iMouse.xy, pos.xy);
+            if(dst < 5.0f) {
+                // Дистанция от мыши до пикселя меньше 5 пикселей - белый цвет
+                fragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+            } else {
+                // Дистанция от мыши до пикселя больше 5 пикселей - черный цвет
+                fragColor = vec4(0, 0, 0, 1.0f);
+            }
+        }
+    } else if(uv.x < 0.5f && uv.y > 0.5f) {
+        // Нижний левый угол
+        // Высвечиваем расстояние от мыши до центра прямоугольника
+        vec2 mouse = iMouse / iResolution; // normalized mouse coordinates
+        if(mouse.x < 0.5f && mouse.y > 0.5f) {
+            float dst = distance(mouse.xy, vec2(0.25f, 0.75f));
+            // Чем ближе мышь к центру, тем светлее цвет
+            fragColor = vec4(1 - (dst * 4), 1 - (dst * 4), 1 - (dst * 4), 1.0f);
+        } else {
+            // Если мышь не в нижнем левом углу - черный цвет
+            fragColor = vec4(0, 0, 0, 1.0f);
+        }
+    } else if(uv.x > 0.5f && uv.y > 0.5f) {
+        // Нижний правый угол
+        // Заливаем цветом зависимым от координаты и времени
+        fragColor = vec4(uv.x * 4 * r, uv.y * 4 * g, b, 1.0f);
+    }
 }
